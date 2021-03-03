eb6d1800467aab19ff0a186d020a385c113fb574
Raviraju Vysyaraju <rvysyaraju@ivycomptech.com>
2021-03-03 14:20:24 +0530

    First commit

---
+++ ReSplash-main/Resplash/Resplash/Navigation/Route.swift
@@ -0,0 +1,11 @@
+//
+//  Navigation.swift
+//  Resplash
+//
+//  Created by Raviraju Vysyaraju on 3/2/21.
+//
+
+enum Route: String {
+    case photos = "PhotosViewController"
+}
+
---
+++ ReSplash-main/Resplash/Resplash/Views/PhotoCell.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoCell.swift
+//  Resplash
+//
+//  Created by Raviraju Vysyaraju on 3/2/21.
+//
+
+import UIKit
+import SDWebImage
+
+class PhotoCell: UICollectionViewCell {
+
+    //MARK: - Outlets
+    @IBOutlet private weak var photoImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    
+    //MARK: - Setup
+    func setupPhoto(_ photo: Photo) {
+        titleLabel.text = photo.title
+        photoImageView.sd_setImage(with: URL(string: photo.url),
+                                   placeholderImage: UIImage(named: "placeholder"))
+    }
+}
